e55261a9c6d9bf48d6351dff9d0cc5cb5d16d6d2
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-07 16:49:51 +0000

    Initial Commit

---
+++ app/src/main/java/tech/peny/kotlinfun/MainActivity.kt
@@ -0,0 +1,15 @@
+package tech.peny.kotlinfun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var textView = findViewById<TextView>(R.id.textView)
+        textView.setText("Heya!");
+    }
+}
